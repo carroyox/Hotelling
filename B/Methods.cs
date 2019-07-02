97622c195f468d068bd1f004c75981ca14a29278
carroyox <46496127+carroyox@users.noreply.github.com>
2019-07-02 00:05:42 -0600

    test EF

--- B/Methods.cs
+++ B/Methods.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
 
-namespace B
+namespace BL
 {
-    public class Class1
+    public class Methods
     {
+        public Methods() {
+           
+        }
+
     }
 }
